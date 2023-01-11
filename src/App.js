8ea82e09cb07fce431b1556eed9c0a1a2215f7fc
samithperera94 <samiththilanka94@gmail.com>
2023-01-11 18:38:39 +0530

    reusing custom hooks

--- src/App.js
+++ src/App.js
@@ -1,43 +1,66 @@
 import React, { useEffect, useState } from 'react';
+import useFetch from './hooks/use-fetch';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-tasks-ee846-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  const transformTasks = (tasksObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
-
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
   };
 
+  const httpData = useFetch(transformTasks);
+    
+  console.error("httpData",httpData)
+  const {isLoading,error,sendRequest: fetchTasks } = httpData;
+    // const abc =   useFetch(
+    //   {url:'https://react-tasks-ee846-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+    //   },transformTasks);
+
+    console.log(typeof fetchTasks,"fetchTasks ??????");
+    console.warn(isLoading,"isLoading ??????",error,"error ??????");
+    // console,error("abc",abc)
+
+
+  // const fetchTasks = async (taskText) => {
+    // setIsLoading(true);
+    // setError(null);
+    // try {
+    //   const response = await fetch(
+    //     'https://react-tasks-ee846-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'
+    //   );
+
+    //   if (!response.ok) {
+    //     throw new Error('Request failed!');
+    //   }
+
+    //   const data = await response.json();
+
+    //   const loadedTasks = [];
+
+    //   for (const taskKey in data) {
+    //     loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+    //   }
+
+    //   setTasks(loadedTasks);
+    // } catch (err) {
+    //   setError(err.message || 'Something went wrong!');
+    // }
+    // setIsLoading(false);
+  // };
+
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({url:'https://react-tasks-ee846-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'},transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- src/hooks/use-fetch.js
+++ src/hooks/use-fetch.js
@@ -1,48 +1,36 @@
-import { useState,useCallback } from "react";
-const useFetch = async() => {
+import { useState, useCallback } from 'react';
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const sendRequest = useCallback(async (requestConfig,applyData) => {
-        // debugger
-        console.log("requestConfig,applyData",requestConfig,applyData)
-        setIsLoading(true);
-        setError(null);
-        try {
-          const response = await fetch(
-            requestConfig.url,{
-                method: requestConfig.method ? requestConfig.method : 'GET',
-                headers:requestConfig.headers ? requestConfig.headers : {},
-                body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
-            }
-          );
-    
-          console.warn("response is",response)
-          if (!response.ok) {
-            throw new Error('Request failed!');
-          }
-    
-          const data = await response.json();
-          console.warn("data is",data)
-    
-         applyData(data);
-        } catch (err) {
-          setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
-      },[]);
-    //   return{
-    //       isLoading:isLoading,
-    //       error:error,
-    //       sendRequest:sendRequest
-    //   }
-    console.log(typeof sendRequest,"::::")
-    return{
-        isLoading,
-        error,
-        sendRequest,
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
     }
-}
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
 
 export default useFetch;
